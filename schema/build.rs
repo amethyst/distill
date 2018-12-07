c0663e1359f26c496528ed93375b4ec29dcdb1a5
Karl Bergström <karl.anton.bergstrom@gmail.com>
2018-12-07 02:41:43 +0100

    Fixed schema build.rs script for new capnp version

--- schema/build.rs
+++ schema/build.rs
@@ -2,7 +2,6 @@ extern crate capnpc;
 
 fn main() {
     capnpc::CompilerCommand::new()
-        .src_prefix("schema")
         .file("schemas/data.capnp")
         .file("schemas/service.capnp")
         .run().expect("schema compiler command");
