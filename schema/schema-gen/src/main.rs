d7a149125cbdecf4c40e90b63ed2a6f5dae3c7d9
Karl Bergström <karl.anton.bergstrom@gmail.com>
2020-02-27 00:19:34 +0100

    removed capnpc dependency from schema crate

--- schema/schema-gen/src/main.rs
+++ schema/schema-gen/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
+    let current_dir = std::env::current_dir().unwrap();
+    let parent_dir = current_dir.join("..");
+    std::env::set_current_dir(parent_dir).unwrap();
     capnpc::CompilerCommand::new()
         .file("schemas/data.capnp")
         .file("schemas/service.capnp")
+        .output_path("src/")
         .run()
         .expect("schema compiler command");
 }
---
+++ schema/src/schemas/mod.rs
@@ -0,0 +1,6 @@
+#[allow(clippy::all)]
+#[allow(dead_code)]
+pub mod data_capnp;
+#[allow(clippy::all)]
+#[allow(dead_code)]
+pub mod service_capnp;
