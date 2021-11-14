0d48ab6f9dc25faf0bf14df3c77a38cdc6264a60
Philip Degarmo <aclysma@gmail.com>
2021-11-13 18:06:28 -0800

    Build fix

--- importer/src/lib.rs
+++ importer/src/lib.rs
@@ -89,7 +89,7 @@ pub trait Importer: Send + 'static {
     /// by storing generated AssetUuids with mappings to format-internal identifiers.
     type State: Serialize + Send + 'static;
 
-    fn default_options(&self, _import_source: ImportSource) -> Option<Self::Options> {
+    fn default_options(&self, _import_source: ImportSource<'_>) -> Option<Self::Options> {
         None
     }
 
@@ -137,7 +137,7 @@ pub trait AsyncImporter: Send + 'static {
     /// by storing generated AssetUuids with mappings to format-internal identifiers.
     type State: Serialize + Send + 'static;
 
-    fn default_options<'a>(&'a self, _import_source: ImportSource) -> Option<Self::Options>;
+    fn default_options<'a>(&'a self, _import_source: ImportSource<'_>) -> Option<Self::Options>;
 
     /// Reads the given bytes and produces assets.
     fn import<'a>(
@@ -181,7 +181,7 @@ impl<T: Importer + Sync> AsyncImporter for T {
         <T as Importer>::version(self)
     }
 
-    fn default_options<'a>(&'a self, import_source: ImportSource) -> Option<Self::Options> {
+    fn default_options<'a>(&'a self, import_source: ImportSource<'_>) -> Option<Self::Options> {
         <T as Importer>::default_options(self, import_source)
     }
 
